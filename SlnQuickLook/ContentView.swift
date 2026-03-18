d72e8d037d637b6690e1355f1405e6fa47581c59
Lewi Uberg <lewiuberg@icloud.com>
2026-03-18 12:49:01 +0100

    Initial Commit

---
+++ SlnQuickLook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlnQuickLook
+//
+//  Created by Lewi Uberg on 18/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlnQuickLook/SlnQuickLookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlnQuickLookApp.swift
+//  SlnQuickLook
+//
+//  Created by Lewi Uberg on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SlnQuickLookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
